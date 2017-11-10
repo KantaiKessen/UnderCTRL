a7ce69e4992a48c9bc3013a37624c2a7298496d1
Kevin Liu <kdliu02@gmail.com>
2017-11-10 09:20:16 -0800

    Unattended Upgrades

--- Ubuntu16.04.5LTS/replace.sh
+++ Ubuntu16.04.5LTS/replace.sh
@@ -11,5 +11,6 @@ cp ./common-auth /etc/pam.d/common-auth
 cp ./common-password /etc/pam.d/common-password
 cp ./login.defs /etc/login.defs
 printf "[SeatDefaults]\ngreeter-show-manual-login=true\ngreeter-hide-users=true\nallow-guest=false\n" > /etc/lightdm/lightdm.conf.d/50-myconfig.conf
-printf "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Download-Upgradable-Packages \"1\";\nAPT::Periodic::AutocleanInterval \"7\";\nAPT::Periodic::Unattended-Upgrade \"1\";"
+printf "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Download-Upgradable-Packages \"1\";\nAPT::Periodic::AutocleanInterval \"7\";\nAPT::Periodic::Unattended-Upgrade \"1\";" > /etc/apt/apt.conf.d/20auto-upgrades
+cp ./50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 #must download clean files folder
